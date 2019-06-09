6def6047a5812878eb6b947bb049f8d890bf7d35
Vladimir <antsupov.vova@gmail.com>
2019-06-09 23:03:33 +0300

    Better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 );
 
